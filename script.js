5becd1b1fee3b3587712a618f02ff29feae16213
Pablo Mattot <mattotpablo@gmail.com>
2021-09-24 14:37:27 +0200

    Set up a gamboard and a gamecontroller module, as well as a player factory

---
+++ script.js
@@ -0,0 +1,17 @@
+const gameboard = (function () {
+    const gameboard = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+    function _render() {
+        
+    }
+})
+
+const player = () => {
+    let name;
+    const setName = () => name = prompt("name?");
+    return { setName };
+}
+
+const gameController = (function () {
+
+})
