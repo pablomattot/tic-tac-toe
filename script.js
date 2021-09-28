e7818ca2095c720df410fc3f8899623d5a1535e6
Pablo Mattot <mattotpablo@gmail.com>
2021-09-28 09:31:31 +0200

    Set up the gameboard in HTML and wrote a javascript function that renders the content of the gameboard array to the webpage

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const gameboard = (function () {
     const gameboard = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 
+    // cache DOM
+    const board = document.querySelector(".gameboard");
+    const cellArray = Array.from(board.children);
+
+    _render();
+
     function _render() {
-        
+        let index;
+        cellArray.forEach(cell => {
+            index = cellArray.indexOf(cell);
+            console.log(gameboard[index]);
+
+            cell.textContent = gameboard[index];
+        })
     }
-})
+})();
 
 const player = () => {
     let name;
@@ -14,4 +26,4 @@ const player = () => {
 
 const gameController = (function () {
 
-})
+})();
